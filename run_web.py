f032e36388c257cd4fc0beebd3b74c0a6064370c
jianzaixuan <63486385+jianzaixuan@users.noreply.github.com>
2020-09-24 12:12:27 +0800

    Add files via upload

---
+++ run_web.py
@@ -0,0 +1,33 @@
+"""
+============================
+Author:丁琴
+Time: 15:34
+E-mail:394597923@qq.com
+Company:南京瓦丁科技限公司
+============================
+"""
+# import unittest
+# from HTMLTestRunnerNew import HTMLTestRunner
+# from common.handle_path import REPORT_DIR,CASE_DIR
+# import os
+# # 加载测试用例套件
+# suite=unittest.defaultTestLoader.discover(CASE_DIR)
+# # 创建运行程序
+# runner=HTMLTestRunner(stream=open(os.path.join(REPORT_DIR,"report.html"), "wb"), title="第一份测试报告")
+# 执行程序
+# runner.run(suite)
+
+
+import pytest
+import os
+
+from common.send_email import send_smg
+
+pytest.main(["-m goodno"])
+
+# pytest.main()
+# 运行测试用例，生成测试报告
+# pytest.main(["-m goodno",'--alluredir=allure_report'])
+# 启动allure服务，查看测试报告
+# os.system('allure serve allure_report')
+# send_smg()
